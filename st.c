557569e6b8339a9aff1114c953ab8a664a589e4c
themamiza <drogoniza@gmail.com>
2026-09-06 16:32:01 +0330

    Clear screen: Fix edge case

--- st.c
+++ st.c
@@ -1931,7 +1931,7 @@ csihandle(void)
 				tclearregion(0, term.c.y+1, term.col-1, term.row-1, 1);
 			break;
 		case 1: /* above */
-			if (term.c.y >= 1)
+			if (term.c.y >= 0)
 				tclearregion(0, 0, term.col-1, term.c.y-1, 1);
 			tclearregion(0, term.c.y, term.c.x, term.c.y, 1);
 			break;
